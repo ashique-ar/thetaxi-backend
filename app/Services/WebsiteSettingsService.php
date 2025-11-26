582dff7621945a44352352e42cefdb1fe52ffe86
Ashique Ar Rasool <asqarrsl@gmail.com>
2025-11-26 12:32:48 +0530

    Update WebsiteSettingsService with all 14 categories and missing fields

--- app/Services/WebsiteSettingsService.php
+++ app/Services/WebsiteSettingsService.php
@@ -76,9 +76,19 @@ class WebsiteSettingsService
             'banner_subheading',
             'banner_award_text',
             'banner_video',
+            'banner_image',
+            'banner_background',
+            'homepage_breadcrumb_image',
             
             // Partner Section
             'partner_section_title',
+            'partner_logo_1',
+            
+            // Featured Vehicles
+            'featured_vehicles_title',
+            'featured_vehicles_description',
+            'featured_vehicles_button_text',
+            'vehicles_view_all_text',
             
             // Feature Section - Text, Icons & Vectors
             'feature_1_title',
@@ -93,52 +103,15 @@ class WebsiteSettingsService
             'feature_cta_text',
             'feature_cta_link',
             'feature_cta_description',
-            
-            // About section settings
-            'about_section_heading',
-            
-            // Offer section settings
-            'offer_section_description',
-            
-            // Counter labels
-            'counter_travel_experience_label',
-            'counter_happy_traveler_label',
-            
-            // Custom travel section
-            'custom_travel_heading',
-            'custom_tours_label',
-            'tour_guide_label',
-            
-            // Commitment section
-            'commitment_description',
-            
-            // FAQ section
-            'faq_section_title',
-            'faq_section_description',
-            
-            // Travel inspirations
-            'travel_story_1_description',
-            'travel_story_2_description',
-            'travel_story_3_title',
             'feature_card_vector',
             'feature_section_vector1',
             'feature_section_vector2',
             
-            // Destinations Section
-            'destinations_section_title',
-            'destinations_section_description',
-            
-            // Packages/Things to Do Section
-            'packages_section_title',
-            'packages_section_description',
-            
-            // Blog Section
-            'blog_section_title',
-            'blog_section_description',
-            
-            // About Section - Text & Images
+            // About section settings (homepage)
+            'about_section_heading',
             'about_section_title',
             'about_section_description',
+            'about_section_image',
             'about_feature_1',
             'about_feature_2',
             'about_feature_3',
@@ -157,31 +130,10 @@ class WebsiteSettingsService
             'counter_people_img_3',
             'counter_people_img_4',
             
-            // Partner/Sponsor Logos
-            'partner_logo_1',
-            'partner_logo_2',
-            'partner_logo_3',
-            'partner_logo_4',
-            'partner_logo_5',
-            'partner_logo_6',
-            'partner_link_1',
-            'partner_link_2',
-            'partner_link_3',
-            'partner_link_4',
-            'partner_link_5',
-            'partner_link_6',
-            
-            // Fallback Images
-            'fallback_destination_image',
-            'fallback_package_image',
-            
-            // Offer Slider Images
+            // Offer section settings
+            'offer_section_description',
             'offer_slider_img_1',
             'offer_slider_img_2',
-            
-            // Packages Section
-            'packages_section_title',
-            'packages_section_description',
             
             // Why Section - Text & Icons
             'why_section_title',
@@ -195,16 +147,16 @@ class WebsiteSettingsService
             'why_feature_icon_3',
             'why_feature_icon_4',
             'why_video_image',
+            
+            // Ratings & Reviews
             'tripadvisor_logo',
             'tripadvisor_stars',
-            
-            // Inspirations Section
-            'inspirations_section_title',
-            'inspirations_section_description',
+            'tripadvisor_review',
             
             // Testimonials Section - Images & Vectors
             'testimonials_section_title',
             'testimonials_section_description',
+            'testimonials_section_image',
             'testimonial_vector',
             'testimonial_author_img_1',
             'testimonial_author_img_2',
@@ -212,77 +164,64 @@ class WebsiteSettingsService
             'testimonial_author_img_4',
             'testimonial_author_img_5',
             
-            // Vector Graphics & Decorative Images
+            // Destinations Section
+            'destinations_section_title',
+            'destinations_section_description',
+            'fallback_destination_image',
+            
+            // Packages/Things to Do Section
+            'packages_section_title',
+            'packages_section_description',
+            'features_section_image',
+            'fallback_package_image',
+            
+            // Blog Section
+            'blog_section_title',
+            'blog_section_description',
             'destination_section_vector',
             'blog_section_vector',
-            'faq_section_vector',
-            
-            // Blog Section Images
             'blog_img_1',
             'blog_img_2',
             'blog_img_3',
             
-            // About Page Settings
-            'about_page_title',
-            'about_page_subtitle',
-            'about_page_description',
-            'about_hero_heading',
-            'about_hero_subheading',
-            'about_hero_image',
-            'about_breadcrumb_image',
-            'founder_name',
-            'founder_title',
-            'founder_signature',
-            'founder_image',
-            'services_section_title',
-            'services_section_description',
-            'service_1_title',
-            'service_1_description',
-            'service_1_icon',
-            'service_2_title',
-            'service_2_description',
-            'service_2_icon',
-            'service_3_title',
-            'service_3_description',
-            'service_3_icon',
-            'service_4_title',
-            'service_4_description',
-            'service_4_icon',
+            // Inspirations Section
+            'inspirations_section_title',
+            'inspirations_section_description',
             
-            // Contact Page Settings
-            'contact_page_title',
-            'contact_page_subtitle',
-            'contact_page_description',
-            'contact_hero_heading',
-            'contact_hero_subheading',
-            'contact_address_1',
-            'contact_address_1_title',
-            'contact_address_1_phone',
-            'contact_address_1_email',
-            'contact_address_1_address',
-            'contact_address_2',
-            'contact_address_2_title',
-            'contact_address_2_phone',
-            'contact_address_2_email',
-            'contact_address_2_address',
-            'contact_address_3',
-            'contact_address_3_title',
-            'contact_address_3_phone',
-            'contact_address_3_email',
-            'contact_address_3_address',
-            'contact_form_title',
-            'contact_form_description',
-            'contact_form_success_message',
-            'contact_form_error_message',
-            'contact_map_latitude',
-            'contact_map_longitude',
-            'contact_map_zoom',
-            'contact_map_title',
-            'contact_breadcrumb_image',
-            'company_name',
-            'company_website',
-            'social_youtube',
-            'social_tiktok',
+            // FAQ section
+            'faq_section_title',
+            'faq_section_description',
+            'faq_section_vector',
+            
+            // Counter labels
+            'counter_travel_experience_label',
+            'counter_happy_traveler_label',
+            
+            // Custom travel section
+            'custom_travel_heading',
+            'custom_tours_label',
+            'tour_guide_label',
+            
+            // Commitment section
+            'commitment_description',
+            
+            // Travel inspirations
+            'travel_story_1_description',
+            'travel_story_2_description',
+            'travel_story_3_title',
+            
+            // Partner/Sponsor Logos
+            'partner_logo_2',
+            'partner_logo_3',
+            'partner_logo_4',
+            'partner_logo_5',
+            'partner_logo_6',
+            'partner_link_1',
+            'partner_link_2',
+            'partner_link_3',
+            'partner_link_4',
+            'partner_link_5',
+            'partner_link_6',
             
             // General Settings
             'homepage_title',
@@ -294,7 +233,12 @@ class WebsiteSettingsService
             'social_facebook',
             'social_twitter',
             'social_instagram',
-            'social_linkedin'
+            'social_linkedin',
+            'social_youtube',
+            'social_tiktok',
+            'company_name',
+            'company_website',
+            'video_section_help_text'
         ];
 
         return $this->getMultiple($types);
@@ -316,7 +260,9 @@ class WebsiteSettingsService
             
             // About Section
             'about_section_title',
-            'about_section_description',
+            'about_section_subtitle',
+            'about_section_description_1',
+            'about_section_description_2',
             'about_feature_1',
             'about_feature_2',
             'about_feature_3',
@@ -338,8 +284,9 @@ class WebsiteSettingsService
             'founder_image',
             
             // Services Section
-            'services_section_title',
-            'services_section_description',
+            'about_services_section_title',
+            'about_journey_section_title',
+            'about_journey_section_description',
             'service_1_title',
             'service_1_description',
             'service_1_icon',
@@ -353,8 +300,23 @@ class WebsiteSettingsService
             'service_4_description',
             'service_4_icon',
             
-            // Breadcrumb
+            // Breadcrumb & Media
             'about_breadcrumb_image',
+            'about_section_image',
+            
+            // Testimonials
+            'about_testimonial_img_1',
+            'about_testimonial_img_2',
+            'about_testimonial_img_3',
+            'about_testimonial_img_4',
+            'about_testimonial_img_5',
+            
+            // Rating Logos
+            'about_tripadvisor_logo',
+            'about_trustpilot_logo',
+            'about_why_travel_title',
+            'about_why_travel_description',
+            'about_partners_title',
             
             // General company info
             'company_phone',
@@ -473,16 +435,17 @@ class WebsiteSettingsService
         $types = [
             // FAQ Page Content
             'faq_page_title',
+            'faq_section_title',
+            'faq_section_description',
             'faq_page_subtitle',
             'faq_breadcrumb_image',
+            'faq_page_banner_image',
+            'faq_section_vector',
             'faq_hero_heading',
             'faq_hero_subheading',
             'faq_hero_background',
             
             // FAQ Section Content
-            'faq_section_title',
-            'faq_section_subtitle',
-            'faq_section_description',
             'faq_search_placeholder',
             'faq_all_categories_text',
             'faq_no_results_text',
@@ -637,14 +600,129 @@ class WebsiteSettingsService
     /**
      * Get all appearance settings
      */
+    public function getHeaderSettings(): array
+    {
+        $types = [
+            'header_need_help_text',
+            'header_cart_text',
+            'header_search_placeholder',
+            'header_quick_search_1',
+            'header_quick_search_2',
+            'header_quick_search_3',
+            'header_quick_search_4',
+            'header_logo_alt_text',
+            'logo_header',
+            'logo_header_alt',
+            'logo_mobile',
+            'logo_mobile_alt'
+        ];
+
+        return $this->getMultiple($types);
+    }
+
+    public function getFooterSettings(): array
+    {
+        $types = [
+            'footer_inquiry_title',
+            'footer_inquiry_subtitle',
+            'footer_whatsapp_label',
+            'footer_mail_label',
+            'footer_call_label',
+            'footer_whatsapp_number',
+            'footer_services_title',
+            'footer_routes_title',
+            'footer_support_title',
+            'footer_copyright_text',
+            'footer_description',
+            // Services Links
+            'footer_services_link_1_text',
+            'footer_services_link_1_url',
+            'footer_services_link_2_text',
+            'footer_services_link_2_url',
+            'footer_services_link_3_text',
+            'footer_services_link_3_url',
+            'footer_services_link_4_text',
+            'footer_services_link_4_url',
+            'footer_services_link_5_text',
+            'footer_services_link_5_url',
+            'footer_services_link_6_text',
+            'footer_services_link_6_url',
+            'footer_services_link_7_text',
+            'footer_services_link_7_url',
+            'footer_services_link_8_text',
+            'footer_services_link_8_url',
+            'footer_services_link_9_text',
+            'footer_services_link_9_url',
+            'footer_services_link_10_text',
+            'footer_services_link_10_url',
+            // Routes Links
+            'footer_routes_link_1_text',
+            'footer_routes_link_1_url',
+            'footer_routes_link_2_text',
+            'footer_routes_link_2_url',
+            'footer_routes_link_3_text',
+            'footer_routes_link_3_url',
+            'footer_routes_link_4_text',
+            'footer_routes_link_4_url',
+            'footer_routes_link_5_text',
+            'footer_routes_link_5_url',
+            'footer_routes_link_6_text',
+            'footer_routes_link_6_url',
+            'footer_routes_link_7_text',
+            'footer_routes_link_7_url',
+            'footer_routes_link_8_text',
+            'footer_routes_link_8_url',
+            'footer_routes_link_9_text',
+            'footer_routes_link_9_url',
+            'footer_routes_link_10_text',
+            'footer_routes_link_10_url',
+            // Support Links
+            'footer_support_link_1_text',
+            'footer_support_link_1_url',
+            'footer_support_link_2_text',
+            'footer_support_link_2_url',
+            'footer_support_link_3_text',
+            'footer_support_link_3_url',
+            'footer_support_link_4_text',
+            'footer_support_link_4_url',
+            'footer_support_link_5_text',
+            'footer_support_link_5_url',
+            'footer_support_link_6_text',
+            'footer_support_link_6_url',
+            'footer_support_link_7_text',
+            'footer_support_link_7_url',
+            'footer_support_link_8_text',
+            'footer_support_link_8_url',
+            'footer_support_link_9_text',
+            'footer_support_link_9_url',
+            'footer_support_link_10_text',
+            'footer_support_link_10_url',
+            // Footer Media
+            'logo_footer',
+            'logo_footer_alt',
+            'footer_breadcrumb_image'
+        ];
+
+        return $this->getMultiple($types);
+    }
+
     public function getAppearanceSettings(): array
     {
         $types = [
             'primary_color',
             'secondary_color',
+            'tertiary_color',
             'logo_header',
+            'logo_header_alt_text',
             'logo_footer',
-            'favicon'
+            'logo_footer_alt_text',
+            'logo_mobile',
+            'logo_mobile_alt_text',
+            'favicon',
+            'favicon_url',
+            'brand_image_1',
+            'brand_image_2',
+            'brand_image_3'
         ];
 
         return $this->getMultiple($types);
@@ -681,14 +759,18 @@ class WebsiteSettingsService
     {
         return [
             'general' => $this->getGeneralSettings(),
+            'homepage' => $this->getHomepageSettings(),
+            'header' => $this->getHeaderSettings(),
+            'footer' => $this->getFooterSettings(),
+            'about' => $this->getAboutPageSettings(),
+            'faq' => $this->getFaqPageSettings(),
             'seo' => $this->getSeoSettings(),
             'social_media' => $this->getSocialMediaSettings(),
             'contact' => $this->getContactPageSettings(),
             'payment' => $this->getPaymentSettings(),
+            'booking' => $this->getBookingSettings(),
             'security' => $this->getSecuritySettings(),
             'email' => $this->getEmailSettings(),
-            'homepage' => $this->getHomepageSettings(),
-            'booking' => $this->getBookingSettings(),
             'appearance' => $this->getAppearanceSettings(),
         ];
     }
