0cd7a8417e2c7db162bfe6b2226265590252190e
Ashique Ar Rasool <asqarrsl@gmail.com>
2025-12-24 08:05:25 +0530

    fix: Format alert close button for better readability in contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -57,14 +57,16 @@
                                             <li>{{ $error }}</li>
                                         @endforeach
                                     </ul>
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
                                 </div>
                             @endif
 
                             @if (session('success'))
                                 <div class="alert alert-success alert-dismissible fade show" role="alert">
                                     {{ session('success') }}
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
                                 </div>
                             @endif
 
