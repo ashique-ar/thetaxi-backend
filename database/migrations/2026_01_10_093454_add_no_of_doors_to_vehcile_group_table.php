06694cc6179abdd23a1f5e14b63083aeab025faf
Ashique Ar Rasool <asqarrsl@gmail.com>
2026-01-10 15:08:57 +0530

    fix: Correct table name in migration for adding 'no_of_doors' field to vehicle groups

--- database/migrations/2026_01_10_093454_add_no_of_doors_to_vehcile_group_table.php
+++ database/migrations/2026_01_10_093454_add_no_of_doors_to_vehcile_group_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('vehcile_group', function (Blueprint $table) {
+        Schema::table('vehcile_groups', function (Blueprint $table) {
             $table->integer('no_of_doors')->nullable()->after('hand_luggages');
         });
     }
