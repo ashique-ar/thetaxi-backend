5d544c8a1eab67032708dc7bcf488e4c1dae1bc3
Ashique Ar Rasool <asqarrsl@gmail.com>
2026-01-10 13:12:52 +0530

    fix: Update booking form JavaScript version to 1.0.08

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -288,7 +288,7 @@
 
     <script src="{{ asset('assets/js/select-dropdown.js') }}"></script>
     <script src="{{ asset('assets/js/custom.js?v=1.0.01') }}"></script>
-    <script src="{{ asset('assets/js/booking-form.js?v=1.0.07') }}"></script>
+    <script src="{{ asset('assets/js/booking-form.js?v=1.0.08') }}"></script>
 
     <!-- Currency Switching JavaScript -->
     <script>
