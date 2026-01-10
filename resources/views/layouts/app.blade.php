4ffce7a313864e2e0be1c0070a56b4b67d9a52b2
Ashique Ar Rasool <asqarrsl@gmail.com>
2026-01-10 13:15:50 +0530

    fix: Update popup display JavaScript version to 1.0.02

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -468,7 +468,7 @@
     </script>
 
     <!-- Popup Display Engine JS -->
-    <script src="{{ asset('assets/js/popup-display.js?v=1.0.01') }}"></script>
+    <script src="{{ asset('assets/js/popup-display.js?v=1.0.02') }}"></script>
 
     @stack('scripts')
 </body>
