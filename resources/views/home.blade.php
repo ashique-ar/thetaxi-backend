454b2d9d845cd31f9d44ccc79c6c87d389359935
Ashique Ar Rasool <asqarrsl@gmail.com>
2026-01-28 09:22:02 +0530

    Refactor "Why Choose Us" section to conditionally display based on settings and improve structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
                     <h1>{{ $settings['banner_heading'] ?? 'All-in-one Travel Booking.' }}</h1>
                     <p>{{ $settings['banner_subheading'] ??
                         'Best travel agency in world-wide & achieve “World
-                                                                                                                                                    Travel Award”' }}
+                                                                                                                                                                        Travel Award”' }}
                     </p>
                     @include('components.booking-form')
                 </div>
@@ -197,116 +197,117 @@
     </div>
     <!-- home4 Offer Slider Section End-->
 
-    <!-- home4 Why Choose Us Section Start-->
-    <div class="home4-why-choose-us-section">
-        <div class="container">
-            <div class="row g-4 justify-content-between align-items-end mb-60 wow animate fadeInDown" data-wow-delay="200ms"
-                data-wow-duration="1500ms">
-                <div class="col-xxl-5 col-xl-6 col-lg-7">
-                    <div class="section-title">
-                        <h2>Why We’re Best Agency</h2>
-                        <p>{{ $settings['offer_section_description'] ?? 'A curated list of the most popular travel packages based on different destinations.' }}
-                        </p>
+    @if ($settings['why_video_image'])
+        <!-- home4 Why Choose Us Section Start-->
+        <div class="home4-why-choose-us-section">
+            <div class="container">
+                <div class="row g-4 justify-content-between align-items-end mb-60 wow animate fadeInDown"
+                    data-wow-delay="200ms" data-wow-duration="1500ms">
+                    <div class="col-xxl-5 col-xl-6 col-lg-7">
+                        <div class="section-title">
+                            <h2>Why We’re Best Agency</h2>
+                            <p>{{ $settings['offer_section_description'] ?? 'A curated list of the most popular travel packages based on different destinations.' }}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 d-flex justify-content-lg-end">
+                        <a href="https://www.tripadvisor.com/" class="single-rating">
+                            <strong>4.5</strong>
+                            <div class="tripadvisor-rating">
+                                <img src="{{ s3_asset($settings['tripadvisor_logo'] ?? 'assets/img/home1/icon/tripadvisor-logo.svg') }}"
+                                    alt="">
+                                <div class="rating-area">
+                                    <span>Reviews</span>
+                                    <img src="{{ s3_asset($settings['tripadvisor_stars'] ?? 'assets/img/home1/icon/tripadvisor-start.svg') }}"
+                                        alt="">
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
-                <div class="col-lg-3 d-flex justify-content-lg-end">
-                    <a href="https://www.tripadvisor.com/" class="single-rating">
-                        <strong>4.5</strong>
-                        <div class="tripadvisor-rating">
-                            <img src="{{ s3_asset($settings['tripadvisor_logo'] ?? 'assets/img/home1/icon/tripadvisor-logo.svg') }}"
-                                alt="">
-                            <div class="rating-area">
-                                <span>Reviews</span>
-                                <img src="{{ s3_asset($settings['tripadvisor_stars'] ?? 'assets/img/home1/icon/tripadvisor-start.svg') }}"
+                <div class="row g-4">
+                    <div class="col-lg-3 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="200ms"
+                        data-wow-duration="1500ms">
+                        <div class="single-feature">
+                            <div class="icon">
+                                <img src="{{ s3_asset($settings['why_feature_icon_1'] ?? 'assets/img/home3/icon/destination-feature-icon1.svg') }}"
                                     alt="">
                             </div>
+                            <h5>{{ $settings['why_feature_1'] ?? 'Customizable Package.' }}</h5>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="400ms"
+                        data-wow-duration="1500ms">
+                        <div class="single-feature">
+                            <div class="icon">
+                                <img src="{{ s3_asset($settings['why_feature_icon_2'] ?? 'assets/img/home3/icon/destination-feature-icon2.svg') }}"
+                                    alt="">
+                            </div>
+                            <h5>{{ $settings['why_feature_2'] ?? '24/7 Support' }}</h5>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="600ms"
+                        data-wow-duration="1500ms">
+                        <div class="single-feature">
+                            <div class="icon">
+                                <img src="{{ s3_asset($settings['why_feature_icon_3'] ?? 'assets/img/home3/icon/destination-feature-icon3.svg') }}"
+                                    alt="">
+                            </div>
+                            <h5>{{ $settings['why_feature_3'] ?? 'Trusted by Thousands' }}</h5>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="800ms"
+                        data-wow-duration="1500ms">
+                        <div class="single-feature">
+                            <div class="icon">
+                                <img src="{{ s3_asset($settings['why_feature_icon_4'] ?? 'assets/img/home3/icon/destination-feature-icon4.svg') }}"
+                                    alt="">
+                            </div>
+                            <h5>{{ $settings['why_feature_4'] ?? 'Local Experties' }}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="why-choose-video-area mb-100">
+            <div class="container">
+                <div class="why-choose-video-wrap">
+                    <img src="{{ $settings['why_video_image'] ? s3_asset($settings['why_video_image']) : asset('assets/img/home4/why-choose-video-img.jpg') }}"
+                        alt="" loading="lazy">
+                    <a data-fancybox="video-player" href="https://www.youtube.com/watch?v=u31qwQUeGuM" class="play-btn">
+                        <i class="bi bi-play-fill"></i>
+                        <div class="waves-block">
+                            <div class="waves wave-1"></div>
+                            <div class="waves wave-2"></div>
+                            <div class="waves wave-3"></div>
                         </div>
                     </a>
-                </div>
-            </div>
-            <div class="row g-4">
-                <div class="col-lg-3 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="200ms"
-                    data-wow-duration="1500ms">
-                    <div class="single-feature">
-                        <div class="icon">
-                            <img src="{{ s3_asset($settings['why_feature_icon_1'] ?? 'assets/img/home3/icon/destination-feature-icon1.svg') }}"
-                                alt="">
-                        </div>
-                        <h5>{{ $settings['why_feature_1'] ?? 'Customizable Package.' }}</h5>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="400ms"
-                    data-wow-duration="1500ms">
-                    <div class="single-feature">
-                        <div class="icon">
-                            <img src="{{ s3_asset($settings['why_feature_icon_2'] ?? 'assets/img/home3/icon/destination-feature-icon2.svg') }}"
-                                alt="">
-                        </div>
-                        <h5>{{ $settings['why_feature_2'] ?? '24/7 Support' }}</h5>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="600ms"
-                    data-wow-duration="1500ms">
-                    <div class="single-feature">
-                        <div class="icon">
-                            <img src="{{ s3_asset($settings['why_feature_icon_3'] ?? 'assets/img/home3/icon/destination-feature-icon3.svg') }}"
-                                alt="">
-                        </div>
-                        <h5>{{ $settings['why_feature_3'] ?? 'Trusted by Thousands' }}</h5>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 wow animate fadeInUp" data-wow-delay="800ms"
-                    data-wow-duration="1500ms">
-                    <div class="single-feature">
-                        <div class="icon">
-                            <img src="{{ s3_asset($settings['why_feature_icon_4'] ?? 'assets/img/home3/icon/destination-feature-icon4.svg') }}"
-                                alt="">
-                        </div>
-                        <h5>{{ $settings['why_feature_4'] ?? 'Local Experties' }}</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="why-choose-video-area mb-100">
-        <div class="container">
-            <div class="why-choose-video-wrap">
-                <img src="{{ $settings['why_video_image'] ? s3_asset($settings['why_video_image']) : asset('assets/img/home4/why-choose-video-img.jpg') }}"
-                    alt="" loading="lazy">
-                <a data-fancybox="video-player" href="https://www.youtube.com/watch?v=u31qwQUeGuM" class="play-btn">
-                    <i class="bi bi-play-fill"></i>
-                    <div class="waves-block">
-                        <div class="waves wave-1"></div>
-                        <div class="waves wave-2"></div>
-                        <div class="waves wave-3"></div>
-                    </div>
-                </a>
-                <div class="contact-wrap">
-                    <div class="contact-area">
-                        <h6>Need to Help? Don’t Hesitate Friendly Collaboarte with Experties</h6>
-                        <div class="single-contact">
-                            <div class="icon">
-                                <svg width="16" height="16" viewBox="0 0 16 16"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <g>
-                                        <path
-                                            d="M15.5646 11.7424L13.3317 9.50954C12.5343 8.7121 11.1786 9.03111 10.8596 10.0678C10.6204 10.7855 9.82296 11.1842 9.10526 11.0247C7.51037 10.626 5.35726 8.55261 4.95854 6.87797C4.71931 6.16024 5.19778 5.36279 5.91548 5.12359C6.95216 4.80461 7.27113 3.44895 6.47369 2.65151L4.24084 0.418659C3.60288 -0.139553 2.64595 -0.139553 2.08774 0.418659L0.572591 1.93381C-0.942555 3.5287 0.73208 7.75516 4.48007 11.5032C8.22807 15.2512 12.4545 17.0056 14.0494 15.4106L15.5646 13.8955C16.1228 13.2575 16.1228 12.3006 15.5646 11.7424Z" />
-                                    </g>
-                                </svg>
-                            </div>
-                            <div class="content">
-                                <span>{{ $settings['header_help_label'] ?? 'Need Help?' }}</span>
-                                <a
-                                    href="tel:{{ preg_replace('/[^0-9+]/', '', $settings['company_phone'] ?? '+1234567890') }}">{{ $settings['company_phone'] ?? '+1 234 567 890' }}</a>
+                    <div class="contact-wrap">
+                        <div class="contact-area">
+                            <h6>Need to Help? Don’t Hesitate Friendly Collaboarte with Experties</h6>
+                            <div class="single-contact">
+                                <div class="icon">
+                                    <svg width="16" height="16" viewBox="0 0 16 16"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <g>
+                                            <path
+                                                d="M15.5646 11.7424L13.3317 9.50954C12.5343 8.7121 11.1786 9.03111 10.8596 10.0678C10.6204 10.7855 9.82296 11.1842 9.10526 11.0247C7.51037 10.626 5.35726 8.55261 4.95854 6.87797C4.71931 6.16024 5.19778 5.36279 5.91548 5.12359C6.95216 4.80461 7.27113 3.44895 6.47369 2.65151L4.24084 0.418659C3.60288 -0.139553 2.64595 -0.139553 2.08774 0.418659L0.572591 1.93381C-0.942555 3.5287 0.73208 7.75516 4.48007 11.5032C8.22807 15.2512 12.4545 17.0056 14.0494 15.4106L15.5646 13.8955C16.1228 13.2575 16.1228 12.3006 15.5646 11.7424Z" />
+                                        </g>
+                                    </svg>
+                                </div>
+                                <div class="content">
+                                    <span>{{ $settings['header_help_label'] ?? 'Need Help?' }}</span>
+                                    <a
+                                        href="tel:{{ preg_replace('/[^0-9+]/', '', $settings['company_phone'] ?? '+1234567890') }}">{{ $settings['company_phone'] ?? '+1 234 567 890' }}</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- home4 Why Choose Us Section End-->
-
+        <!-- home4 Why Choose Us Section End-->
+    @endif
     <!-- home4 Testimonial Section Start-->
     @if ($testimonials && $testimonials->count() > 0)
         <div class="home4-testimonial-section mb-100">
