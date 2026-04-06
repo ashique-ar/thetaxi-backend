f35a9e11d1dfd1ddf4587d06bfa5e48498892492
Ashique Ar Rasool <asqarrsl@gmail.com>
2026-04-06 16:56:39 +0530

    feat: Update minimum location update interval from 10 to 7 seconds

--- app/Services/Driver/LocationService.php
+++ app/Services/Driver/LocationService.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\DB;
  */
 class LocationService
 {
-    private const MIN_UPDATE_INTERVAL_SECONDS = 10;
+    private const MIN_UPDATE_INTERVAL_SECONDS = 7;
 
     public function __construct(
         private ?WaitingTimeService $waitingTimeService = null
