88404372994b8f7426833996ab0aca875d9299e2
Ashique Ar Rasool <asqarrsl@gmail.com>
2026-04-07 16:50:24 +0530

    feat: Set minimum update interval for location updates to zero seconds

--- app/Services/Driver/LocationService.php
+++ app/Services/Driver/LocationService.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\DB;
  */
 class LocationService
 {
-    private const MIN_UPDATE_INTERVAL_SECONDS = 1;
+    private const MIN_UPDATE_INTERVAL_SECONDS = 0;
 
     public function __construct(
         private ?WaitingTimeService $waitingTimeService = null
