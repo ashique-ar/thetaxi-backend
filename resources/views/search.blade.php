f00854f90d469e9c4d0298ef2af1164ea26cf7bf
Ashique Ar Rasool <asqarrsl@gmail.com>
2026-09-17 10:10:49 +0530

    feat(vehicle-card): update column class for vehicle card layout in search results

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -307,7 +307,7 @@
                             $isRecommended = $result['recommended'] ?? false;
                         @endphp
 
-                        <div class="{{!is_theme('theme-04') ? 'col-md-3' : ''}} col-md-4 col-sm-6 col-6 vehicle-card-wrapper"
+                        <div class="{{!is_theme('theme-04') ? 'col-lg-3' : ''}} col-md-4 col-sm-6 col-6 vehicle-card-wrapper"
                             data-vehicle-group="{{ $result['id'] }}" data-price="{{ $pricing['base_amount'] ?? 0 }}"
                             data-name="{{ $result['name'] ?? 'Unknown Vehicle' }}">
                             <x-vehicle-card :vehicle="$result" :pricing="$pricing" :enhancedPricing="$enhancedPricing" :serviceFeatures="$serviceFeatures"
